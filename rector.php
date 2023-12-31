7f4014925aa2b7f8c05904be21a6459a358929cf
Michael Kettel <mkettel@gmail.com>
2023-12-31 21:08:58 +0100

    wip

--- rector.php
+++ rector.php
@@ -25,13 +25,13 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         Typo3LevelSetList::UP_TO_TYPO3_11,
         //˚SetList::DEAD_CODE,
-        SetList::PHP_81,
+        SetList::PHP_82,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
     ]);
 
     // Define your target version which you want to support
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
 
     // If you only want to process one/some TYPO3 extension(s), you can specify its path(s) here.
     // If you use the option --config change __DIR__ to getcwd()
@@ -58,8 +58,6 @@ return static function (RectorConfig $rectorConfig): void {
         //__DIR__ . '/public/*',
         __DIR__ . '/.github/*',
         __DIR__ . '/.Build/*',
-        '**/typo3/index.php',
-        '**/public/index.php',
         NameImportingPostRector::class => [
             //'ext_localconf.php',
             //'ext_tables.php',
