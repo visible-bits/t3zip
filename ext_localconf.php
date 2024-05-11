3d9b64cfaeab355b02792c36f496e744066b0e70
Michael Kettel <mkettel@gmail.com>
2024-05-11 18:26:28 +0200

    prepare TYPO3 v12

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,5 +6,7 @@ use Vibi\T3zip\ContextMenu\ItemProviders\ItemProvider;
 
 defined('TYPO3') || die();
 
+// not needed anymore in TYPO3 v12
+// https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ApiOverview/Backend/ContextualMenu.html
 $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1703336651] =
     ItemProvider::class;
